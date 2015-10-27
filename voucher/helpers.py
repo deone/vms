db2f28caa256d2c4b5924a61d1f50c973f443f3c
Dayo Osikoya <alwaysdeone@gmail.com>
2015-10-27 10:43:55 +0100

    Make Voucher.date_created default to timezone.now.

--- voucher/helpers.py
+++ voucher/helpers.py
@@ -1,4 +1,3 @@
-from django.utils import timezone
 from django.conf import settings
 
 import random
@@ -10,4 +9,4 @@ def generate_vouchers(price, quantity, batch):
     chars = string.digits
     for i in range(int(quantity)):
         pin = ''.join(random.choice(chars) for _ in range(settings.PIN_LENGTH))
-        Voucher.objects.create(pin=pin, value=price, date_created=timezone.now(), batch=batch)
+        Voucher.objects.create(pin=pin, value=price, batch=batch)
--- voucher/models.py
+++ voucher/models.py
@@ -43,6 +43,6 @@ class Voucher(models.Model):
     # Use id as serial number
     pin = models.CharField(max_length=14)
     value = models.SmallIntegerField()
-    date_created = models.DateTimeField()
+    date_created = models.DateTimeField(default=timezone.now)
     is_valid = models.BooleanField(default=True) # this can be True or False
     batch = models.ForeignKey(Batch)
