02fedaf808b1531e349e3f623ab83360e9562320
Dayo Osikoya <alwaysdeone@gmail.com>
2015-11-15 22:41:08 +0100

    More template markup. Add form-control class to GenerateVoucherForm.

--- vouchers/forms.py
+++ vouchers/forms.py
@@ -4,8 +4,8 @@ from .models import Voucher, Batch
 from .helpers import generate_vouchers
 
 class GenerateVoucherForm(forms.Form):
-    price = forms.ChoiceField(label='Price', choices=Voucher.PRICE_CHOICES)
-    quantity = forms.ChoiceField(label='Quantity', choices=Voucher.QUANTITY_CHOICES)
+    price = forms.ChoiceField(label='Price', choices=Voucher.PRICE_CHOICES, widget=forms.Select(attrs={'class': 'form-control'}))
+    quantity = forms.ChoiceField(label='Quantity', choices=Voucher.QUANTITY_CHOICES, widget=forms.Select(attrs={'class': 'form-control'}))
 
     def save(self):
         price = self.cleaned_data['price']
