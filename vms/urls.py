8300298d6114f634e88d07bb58be0b4924aebc19
Dayo Osikoya <alwaysdeone@gmail.com>
2015-11-15 19:41:01 +0100

    Login template markup. Subclass AuthenticationForm so that we can add the 'form-control' class.

--- vms/urls.py
+++ vms/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.contrib.auth.forms import AuthenticationForm
+
+from accounts.forms import VMSAuthenticationForm
 
 urlpatterns = [
-    url(r'^$', auth_views.login, {'template_name': 'accounts/login.html', 'authentication_form': AuthenticationForm}, name='login'),
+    url(r'^$', auth_views.login, {'template_name': 'accounts/login.html', 'authentication_form': VMSAuthenticationForm}, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'accounts/', include('accounts.urls', namespace="accounts")),
     url(r'^vouchers/', include('vouchers.urls', namespace="vouchers")),
