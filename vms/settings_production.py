a17455771726ba7889de53edef4a919fb5aa4dc7
Dayo Osikoya <alwaysdeone@gmail.com>
2016-02-24 21:56:59 +0100

    Update production settings.

--- vms/settings_production.py
+++ vms/settings_production.py
@@ -1,5 +1,17 @@
-from settings_test import *
+from settings import *
 
 DEBUG = False
 
 IP = '154.117.8.18'
+
+ALLOWED_HOSTS = [IP]
+
+BILLING_URL = "http://" + IP + ":8080/"
+
+PACKAGES_URL = BILLING_URL + "packages/"
+
+PACKAGE_INSERT_URL = PACKAGES_URL + "insert/"
+
+PACKAGE_DELETE_URL = PACKAGES_URL + "delete/"
+
+INSTANT_VOUCHER_INSERT_URL = PACKAGES_URL + "insert_vouchers/"
