910cbe5e2e8895cd5ff28820f86b8dbf47f5faab
Oladayo Osikoya <alwaysdeone@gmail.com>
2016-11-12 10:07:38 +0000

    Close URLs with appropriate characters.

--- vouchers/urls.py
+++ vouchers/urls.py
@@ -6,14 +6,14 @@ from .forms import GenerateStandardVoucherForm, GenerateInstantVoucherForm
 urlpatterns = [
     url(r'^download/(?P<pk>\d+)/$', views.download, name='download'),
     url(r'^sell/$', views.sell, name='sell'),
-    url(r'^generate/standard$', views.generate,
+    url(r'^generate/standard/$', views.generate,
       {
         'template': 'vouchers/generate_standard.html',
         'voucher_form': GenerateStandardVoucherForm,
         'redirect_to': 'vouchers:generate_standard',
       },
       name='generate_standard'),
-    url(r'^generate/instant$', views.generate,
+    url(r'^generate/instant/$', views.generate,
       {
         'template': 'vouchers/generate_instant.html',
         'voucher_form': GenerateInstantVoucherForm,
@@ -24,7 +24,7 @@ urlpatterns = [
     url(r'^insert/$', views.insert_stub, name='insert'),
     url(r'^delete/$', views.delete_stub, name='delete'),
     url(r'^redeem/$', views.redeem, name='redeem'),
-    url(r'^invalidate/', views.invalidate, name='invalidate'),
-    url(r'^fetch/', views.fetch_vouchers, name='fetch_vouchers'),
-    url(r'^values/', views.fetch_voucher_values, name='fetch_voucher_values'),
+    url(r'^invalidate/$', views.invalidate, name='invalidate'),
+    url(r'^fetch/$', views.fetch_vouchers, name='fetch_vouchers'),
+    url(r'^values/$', views.fetch_voucher_values, name='fetch_voucher_values'),
 ]
