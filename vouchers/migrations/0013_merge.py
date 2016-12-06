fe9ef46449da806779556cb0234602d4d8a44ff6
Oladayo Osikoya <alwaysdeone@gmail.com>
2016-12-06 11:25:27 +0000

    Merge migrations.

---
+++ vouchers/migrations/0013_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('vouchers', '0012_auto_20161126_1008'),
+        ('vouchers', '0009_auto_20161205_1943'),
+    ]
+
+    operations = [
+    ]
