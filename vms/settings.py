d28add66e2f7e17a9dca1801d09852438ba29fb4
Oladayo Osikoya <alwaysdeone@gmail.com>
2016-12-16 02:32:20 +0000

    Change email settings.

--- vms/settings.py
+++ vms/settings.py
@@ -133,11 +133,12 @@ USERNAME_LENGTH = 4
 PASSWORD_LENGTH = 4
 
 # Email settings
-DEFAULT_FROM_EMAIL = 'Spectra VMS<noreply@spectrawireless.com>'
-EMAIL_HOST = '74.55.86.74'
-EMAIL_PORT = 25
-EMAIL_HOST_USER = 'deone'
-EMAIL_HOST_PASSWORD = '@dune369'
+DEFAULT_FROM_EMAIL = 'Spectra VMS<incisiaappmailer@gmail.com>'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'incisiaappmailer@gmail.com'
+EMAIL_HOST_PASSWORD = '#$gh78@%'
+EMAIL_USE_TLS = True
 
 # Report recipients
 VOUCHER_GEN_REPORT_TO = ['alwaysdeone@gmail.com']
