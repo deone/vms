98f269b6e22c6c1ef71d72156428599719b2d8df
Oladayo Osikoya <alwaysdeone@gmail.com>
2017-02-17 11:55:50 +0000

    Remove port number from billing URL.

--- vouchers/urls.py
+++ vouchers/urls.py
@@ -7,19 +7,19 @@ urlpatterns = [
     url(r'^download/(?P<pk>\d+)/$', views.download, name='download'),
     url(r'^sell/$', views.sell, name='sell'),
     url(r'^generate/standard/$', views.generate,
-      {
+    {
         'template': 'vouchers/generate_standard.html',
         'voucher_form': GenerateStandardVoucherForm,
         'redirect_to': 'vouchers:generate_standard',
-      },
-      name='generate_standard'),
+    },
+    name='generate_standard'),
     url(r'^generate/instant/$', views.generate,
-      {
+    {
         'template': 'vouchers/generate_instant.html',
         'voucher_form': GenerateInstantVoucherForm,
         'redirect_to': 'vouchers:generate_instant',
-      },
-      name='generate_instant'),
+    },
+    name='generate_instant'),
     url(r'^batches/$', views.BatchList.as_view(), name='batches'),
     url(r'^insert/$', views.insert_stub, name='insert'),
     url(r'^delete/$', views.delete_stub, name='delete'),
