01ac462cf3f15c478cd230a86b18faba4ad3b8fe
Oladayo Osikoya <alwaysdeone@gmail.com>
2017-02-17 12:20:56 +0000

    Disable Vend model in admin.

--- vouchers/admin.py
+++ vouchers/admin.py
@@ -9,4 +9,4 @@ class BatchAdmin(admin.ModelAdmin):
     list_display = ('date_created', 'value', 'quantity', 'voucher_type')
 
 admin.site.register(Batch, BatchAdmin)
-admin.site.register(Vend, VendAdmin)
+# admin.site.register(Vend, VendAdmin)
