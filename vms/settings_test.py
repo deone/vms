f6a78a575d23255aa0be57a26c0b617e514bfade
Oladayo Osikoya <alwaysdeone@gmail.com>
2017-05-05 13:08:52 +0000

    Point test environment to test billing-v1 for instant vouchers. Change port number to 8008 since this is allowed by selinux.

--- vms/settings_test.py
+++ vms/settings_test.py
@@ -1,14 +1,14 @@
 from settings import *
 
-IP = '154.117.8.19'
+IP = '154.117.12.5'
 ALLOWED_HOSTS = [IP]
 
 # Billing settings
-BILLING_URL = "http://" + IP + "/"
-PACKAGES_URL = BILLING_URL + "packages/"
-PACKAGE_INSERT_URL = PACKAGES_URL + "insert/"
-PACKAGE_DELETE_URL = PACKAGES_URL + "delete/"
-INSTANT_VOUCHER_INSERT_URL = PACKAGES_URL + "insert_vouchers/"
+BILLING_URL = 'http://154.117.8.19/'
+PACKAGES_URL = BILLING_URL + 'packages/'
+PACKAGE_INSERT_URL = PACKAGES_URL + 'insert/'
+PACKAGE_DELETE_URL = PACKAGES_URL + 'delete/'
+INSTANT_VOUCHER_INSERT_URL = PACKAGES_URL + 'insert_vouchers/'
 
 # Report recipients
 VOUCHER_GEN_REPORT_TO = ['alwaysdeone@gmail.com']
