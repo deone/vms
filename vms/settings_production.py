9765a189ea22174ce9d3d2f3a0e3257588667c35
Oladayo Osikoya <alwaysdeone@gmail.com>
2017-05-05 13:19:16 +0000

    Add production IPs.

--- vms/settings_production.py
+++ vms/settings_production.py
@@ -2,11 +2,11 @@ from settings import *
 
 DEBUG = False
 
-IP = '154.117.8.18'
+IP = '154.117.12.5'
 ALLOWED_HOSTS = [IP]
 
 # Billing settings
-BILLING_URL = "http://" + IP + "/"
+BILLING_URL = "http://154.117.8.21/"
 PACKAGES_URL = BILLING_URL + "packages/"
 PACKAGE_INSERT_URL = PACKAGES_URL + "insert/"
 PACKAGE_DELETE_URL = PACKAGES_URL + "delete/"
